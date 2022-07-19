5c377042d50b64173287b8071a7427f639124b28
artmkv <kurasov.art@gmail.com>
2022-07-19 19:11:33 +0200

    add Feign client

--- src/main/java/com/solbegsoft/beersapi/BeersApiApplication.java
+++ src/main/java/com/solbegsoft/beersapi/BeersApiApplication.java
@@ -3,7 +3,10 @@ package com.solbegsoft.beersapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.event.EventListener;
 
+@EnableFeignClients("com.solbegsoft.beersapi")
 @EnableEurekaClient
 @SpringBootApplication
 public class BeersApiApplication {
---
+++ src/main/java/com/solbegsoft/beersapi/controllers/TestFeignController.java
@@ -0,0 +1,31 @@
+package com.solbegsoft.beersapi.controllers;
+
+
+import com.solbegsoft.beersapi.models.response.ResponseApi;
+import com.solbegsoft.beersapi.repositories.BeersFeignClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Beers Controller
+ */
+@RestController
+@RequestMapping("beers-api/v1/test")
+@RequiredArgsConstructor
+public class TestFeignController {
+
+    /**
+     * @see BeersFeignClient
+     */
+    private final BeersFeignClient beersFeignClients;
+
+    @GetMapping
+    public ResponseApi<String> getTestAuthApi() {
+
+        String one = "Message from Beers API";
+        String result = beersFeignClients.getAll();
+        return new ResponseApi<>(one + "Response from FeignController:  " + result);
+    }
+}
